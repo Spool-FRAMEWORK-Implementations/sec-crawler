77cf08dc44d5526de0668d6b718811c6b67f96bd
javcastilla <javier.casno@gmail.com>
2026-05-09 17:58:02 +0100

    feat: implemented synthea yaml

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 mvn clean package
-docker stop crawler
-docker rm crawler
-docker build -t crawler .
-docker run -d --name crawler -p :8080 crawler
+docker stop synthea
+docker rm synthea
+docker build -t synthea .
+docker run -d --name synthea -p :8080 -v "C:/Users/Javito/spool:/host_spool" synthea
